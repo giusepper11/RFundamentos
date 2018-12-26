f5afcca8b4eeeb53c26b01e9d5384f94ff58c50d
Giuseppe Rosa <giusepper11@gmail.com>
2018-12-25 23:48:56 -0200

    Script 1-comandos basicos

--- Parte1/01-RStudio.R
+++ Parte1/01-RStudio.R
@@ -5,3 +5,33 @@ contributors()
 
 # lincença
 sessionInfo()
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+# Criar gráficos
+plot(1:10)
+hist(rnorm(10)) # rnorm > cria uma distribuiçâo normal
+
+# instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+
+# carregar o pacote
+library(ggplot2)
+
+# decarregar o pacote
+detach(package:ggplot2)
+
+# Se souber o nome da função
+help(mean)
+?mean
+
+# Se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+
